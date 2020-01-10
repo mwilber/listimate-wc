79f6cf680e4db473da12d6f2d62fe8b0bfa5f95d
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2020-01-10 15:17:39 -0500

    GZ  DataStore phase 1

--- src/GzDataStore.js
+++ src/GzDataStore.js
@@ -1,12 +1,37 @@
 export class GzDataStore{
     constructor(storeName, storeDefault){
+        this.dataStoreName = storeName;
         this.dataStore = storeDefault;
         // Restore from localStorage
         if(window.localStorage){
             let storedData = window.localStorage.getItem(storeName);
-            if(storedData) this.dataStore = storedData;
+            if(storedData) this.dataStore = JSON.parse(storedData);
         }
+
+        // Set up event listeners
+        document.addEventListener('GzDataBind', function(evt){
+
+        }.bind(this));
+
+        document.addEventListener('GzDataUpdate', function(evt){
+            console.log("TCL: GzDataStore -> constructor -> this.dataStore", this.dataStore)
+            try{
+                let targetRef = this._resolve(evt.detail.target, this.dataStore ) //eval('this.dataStore.'+evt.detail.target);
+                if(targetRef){
+                    Object.assign(targetRef, evt.detail.payload)
+                    // Copy the new dataStore to localStorage
+                    if(window.localStorage) window.localStorage.setItem(this.dataStoreName, JSON.stringify(this.dataStore));
+                }
+            }catch(e){
+                console.error('GzDataStore error: '+e.message);
+            }
+            
+        }.bind(this));
     }
 
-    
+    _resolve(path, obj) {
+        return path.split('.').reduce(function(prev, curr) {
+            return prev ? prev[curr] : null
+        }, obj || self)
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { GzDataStore } from './GzDataStore';
 
 let dataStore = new GzDataStore('listimate', {
-    lists: []
+    lists: ['blah']
 });
 
 let repeater = document.querySelector('gz-for');
+
+document.dispatchEvent(new CustomEvent('GzDataUpdate', {
+    detail:{
+        target: 'lists',
+        payload: ['one', {name: 'Walmart', total: 33.33, items: ['blah']}, 'three']
+    }
+}));

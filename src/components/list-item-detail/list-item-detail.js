6811012eb412dd4ebbc8704dc7f2c3fdfae8fc16
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2020-01-17 23:27:39 -0500

    Delete feature in item detail

--- src/components/list-item-detail/list-item-detail.js
+++ src/components/list-item-detail/list-item-detail.js
@@ -6,6 +6,10 @@ window.customElements.define('list-item-detail', class extends GzDataElement {
   constructor(){
     super();
 
+    this._clearDataSet();
+  }
+
+  _clearDataSet(){
     this._dataSet = {
       name:'', 
       number: ''
@@ -27,7 +31,16 @@ window.customElements.define('list-item-detail', class extends GzDataElement {
         <p>Number:  ${number}</p>
 
         <code>${dataSet}</code>
+        <button class="delete">&#10060;</button>
       `;
+
+      this.shadowRoot.querySelector('.delete').addEventListener('click', function(evt){
+        document.querySelector('list-items').deleteItem(parseInt(this.getAttribute('idx')));
+        this.setAttribute("dataBind", "");
+        this.setAttribute("idx", "");
+        this._clearDataSet();
+        this.render();
+      }.bind(this));
     }
   }
 });
--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -21,15 +21,11 @@ window.customElements.define('list-item', class extends GzDataElement {
         </style>
         
         <h2>name: ${name}</h2>
-        <button class="delete">&#10060;</button>
       `;
 
       this.shadowRoot.querySelector('h2').addEventListener('click', function(evt){
         document.querySelector('list-item-detail').setAttribute('dataBind', this.getAttribute('databind'));
-      }.bind(this));
-
-      this.shadowRoot.querySelector('.delete').addEventListener('click', function(evt){
-        document.querySelector('list-items').deleteItem(parseInt(this.getAttribute('idx')));
+        document.querySelector('list-item-detail').setAttribute('idx', this.getAttribute('idx'));
       }.bind(this));
     }
 });

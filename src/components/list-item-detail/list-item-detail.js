088208676509f7babad59cd14cf8f42f7148bc63
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2020-02-01 22:55:35 -0500

    fix detail layout

--- src/components/list-item-detail/list-item-detail.js
+++ src/components/list-item-detail/list-item-detail.js
@@ -30,11 +30,17 @@ window.customElements.define('list-item-detail', class extends GzDataElement {
         
         <h1>${name}</h1>
         <div class="input-group">
-          <label>$</label><input name="price" type="number" value="${price}"/>
-          <div style="flex: 100%;"></div>
-          <label>qty</label><input name="quantity" type="number" value="${quantity}"/>
+            <label>$</label>
+            <input name="price" type="number" value="${price}"/>
+            <div class="inc-group"></div>
+            <label>qty</label>
+            <input name="quantity" type="number" value="${quantity}"/>
+            <div class="inc-group">
+              <button>+</button>
+              <button>-</button>
+            </div>
         </div>
-        <div class="button-broup">
+        <div class="button-group">
           <button class="save">
             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
           </button>
@@ -45,7 +51,7 @@ window.customElements.define('list-item-detail', class extends GzDataElement {
             <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="trash-alt" class="svg-inline--fa fa-trash-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M268 416h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12zM432 80h-82.41l-34-56.7A48 48 0 0 0 274.41 0H173.59a48 48 0 0 0-41.16 23.3L98.41 80H16A16 16 0 0 0 0 96v16a16 16 0 0 0 16 16h16v336a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128h16a16 16 0 0 0 16-16V96a16 16 0 0 0-16-16zM171.84 50.91A6 6 0 0 1 177 48h94a6 6 0 0 1 5.15 2.91L293.61 80H154.39zM368 464H80V128h288zm-212-48h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12z"></path></svg>
           </button>
         </div>
-        <code>${dataSet}</code>
+        <!--<code>${dataSet}</code>-->
       `;
 
       this.shadowRoot.querySelector('.save').addEventListener('click', function(evt){

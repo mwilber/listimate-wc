c07757379208e3f6949bfc2d7973df21dc0547e5
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2020-03-21 21:28:41 -0400

    Add service worker with basic cache

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,17 @@
-/**
- * Service Worker boilerplate
- * 
- * This service worker has the bare minimum of features required 
- * to qualify for PWA installation. Use this as a starting point
- * to build out your own caching strategy and other PWA features.
- */
+var CACHE_VERSION = '1';	// Increment this by 1 to delete and recreate all of the current caches
+var CACHE_NAME_STATIC = 'gz-lst-static';
+var CACHE_NAME_DYNAMIC = 'gz-lst-dynamic'; 
+
+var FIREBASE_DOMAIN = "https://www.googleapis.com";
 
 self.addEventListener('install', function(event){
     console.log('[SW] installing...');
-    event.waitUntil(caches.open('static')
+    event.waitUntil(caches.open(CACHE_NAME_STATIC+'-v'+CACHE_VERSION)
         .then(function(cache){
             console.log('[SW] precaching');
             cache.addAll([
                 '/',
-                '/index.html',
-                '/app-shell.css'
+                '/index.html'
             ]);
         }));
 });
@@ -24,14 +21,33 @@ self.addEventListener('activate', function(event){
     self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event){
-    event.respondWith(
+
+self.addEventListener('fetch', (event) => {
+	console.log('[SW]', 'fetch event', event);
+
+	event.respondWith(
         caches.match(event.request)
             .then(function(response){
                 if(response){
                     return response;
                 }else{
-                    return fetch(event.request);
+                    return fetch(event.request)
+                        .then(function(res){
+                            if( 
+                                !event.request.url.startsWith('chrome-extension://') &&
+                                !event.request.url.includes('sockjs-node') &&
+                                !event.request.url.includes(FIREBASE_DOMAIN)
+                            ){
+                                console.log("event.request.url", event.request.url)
+                                
+                                return caches.open(CACHE_NAME_DYNAMIC+'-v'+CACHE_VERSION)
+                                    .then(function(cache){
+                                        cache.put(event.request.url, res.clone());
+                                    });
+                            }else{
+                                return res;
+                            }
+                        });
                 }
             })
     );
--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+/**
+ * Import PWA helper files
+ * 
+ * These file are optional. app-shell.css will not be packed into the js bundle and is linked 
+ * separately in index.html. Use for initial styling to be displayed as JavaScripts load. serviceWorkerRegistration 
+ * contains registration code for service-worker.js For more information on service workers and Progressive Web Apps 
+ * check out the GreenZeta 10 minute PWA example at https://github.com/mwilber/gz-10-minute-pwa
+ */ 
+import './serviceWorkerRegistration';
+
 import { GzDataStoreFirebase } from './GzDataStoreFirebase';
 
 

7e8bdd7d38fc8ca08782725f09a3084494a5ee82
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2020-03-21 21:48:58 -0400

    Clean up old caches with version number changes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = '1';	// Increment this by 1 to delete and recreate all of the current caches
+var CACHE_VERSION = '2';	// Increment this by 1 to delete and recreate all of the current caches
 var CACHE_NAME_STATIC = 'gz-lst-static';
 var CACHE_NAME_DYNAMIC = 'gz-lst-dynamic'; 
 
@@ -18,6 +18,18 @@ self.addEventListener('install', function(event){
 
 self.addEventListener('activate', function(event){
     console.log('[SW] activating...');
+    event.waitUntil(
+        caches.keys()
+            .then(function(keyList) {
+                return Promise.all(keyList.map(function(key) {
+                if (key !== (CACHE_NAME_STATIC+'-v'+CACHE_VERSION) &&
+                    key !== (CACHE_NAME_DYNAMIC+'-v'+CACHE_VERSION)
+                ) {
+                    return caches.delete(key);
+                }
+                }));
+            })
+    );
     self.clients.claim();
 });
 
@@ -38,11 +50,11 @@ self.addEventListener('fetch', (event) => {
                                 !event.request.url.includes('sockjs-node') &&
                                 !event.request.url.includes(FIREBASE_DOMAIN)
                             ){
-                                console.log("event.request.url", event.request.url)
                                 
                                 return caches.open(CACHE_NAME_DYNAMIC+'-v'+CACHE_VERSION)
                                     .then(function(cache){
                                         cache.put(event.request.url, res.clone());
+                                        return res;
                                     });
                             }else{
                                 return res;

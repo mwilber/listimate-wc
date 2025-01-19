5099a62deee0783e773319ade22adf5542574362
Matthew Wilber <mwilber@gmail.com>
2025-01-19 16:13:21 -0500

    Move firebase credentials out of project.  - Retire demo account

--- src/GzDataStoreFirebase.js
+++ src/GzDataStoreFirebase.js
@@ -3,6 +3,8 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 import { GzDataStore } from './GzDataStore';
+import { firebaseConfig, firebaseUsername, firebasePassword } from './firebaseCredentials';
+
 
 export class GzDataStoreFirebase extends GzDataStore{
     constructor(storeName, storeDefault){
@@ -11,15 +13,6 @@ export class GzDataStoreFirebase extends GzDataStore{
         this.dbRef = null;
         this.userId = null;
 
-        var firebaseConfig = {
-            apiKey: "AIzaSyDMTmZp8ApiHFQRNwnHV_LlvQcPGKUFAb0",
-            authDomain: "listimate.firebaseapp.com",
-            databaseURL: "https://listimate.firebaseio.com",
-            projectId: "listimate",
-            storageBucket: "listimate.appspot.com",
-            messagingSenderId: "564163854220",
-            appId: "1:564163854220:web:770c201ecc2255ff2160a9"
-        };
         // Initialize Firebase
         firebase.initializeApp(firebaseConfig);
 
@@ -44,7 +37,7 @@ export class GzDataStoreFirebase extends GzDataStore{
             }
         }.bind(this));
         
-        firebase.auth().signInWithEmailAndPassword("matt@mwilber.com", "polloloco").catch(function(error) {
+        firebase.auth().signInWithEmailAndPassword(firebaseUsername, firebasePassword).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;

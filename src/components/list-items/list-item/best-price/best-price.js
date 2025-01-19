42ca715140c23536633dd06d79ac70a3b353677f
Matthew Wilber <mwilber@gmail.com>
2025-01-18 20:32:11 -0500

    Always display store price in best-price - Only display best price when not store price.

--- src/components/list-items/list-item/best-price/best-price.js
+++ src/components/list-items/list-item/best-price/best-price.js
@@ -10,6 +10,16 @@ window.customElements.define('best-price', class extends GzDataElement {
     }
     
     render(){
+      if (!this.dataset.store) return;
+
+      const displayPrice = {
+        name: this.dataset.store,
+        price: this._dataSet[this.dataset.store]
+      };
+      let bestPrice = null;
+      let bestPriceStr = '';
+
+      if (!displayPrice.price && displayPrice.price !== 0) return;
       if (typeof this._dataSet === 'undefined') {
         console.log('no price prop for ', this.getAttribute('databind'), this._dataSet)
         // Add a placeholder under the name so the price can be added later
@@ -17,19 +27,19 @@ window.customElements.define('best-price', class extends GzDataElement {
         return;
       } else if(!this._dataSet) return;
 
-      let displayPrice = null;
-      
       Object.keys(this._dataSet).forEach(
         (name) => {
           const price = this._dataSet[name];
           if (price !== 'x' && (!displayPrice || displayPrice.price > price))
-            displayPrice = {name: name, price: price}
+            bestPrice = {name: name, price: price}
         }
       );
 
-      if (displayPrice === null) return;
+      if(bestPrice && bestPrice.name !== displayPrice.name) {
+        bestPriceStr = `&nbsp;&nbsp;&gt;&nbsp;&nbsp;${bestPrice.name} $${bestPrice.price}`;
+      }
 
-      this.dataset.store = displayPrice.name;
+      //this.dataset.store = displayPrice.name;
       this.dataset.price = displayPrice.price;
 
       this.shadowRoot.innerHTML = `
@@ -40,6 +50,7 @@ window.customElements.define('best-price', class extends GzDataElement {
         <div class="price-tag">
           <span class="name">${displayPrice.name}</span>
           <span class="price">$${displayPrice.price}</span>
+          <span class="best-price">${bestPriceStr}</span>
         </div>
       `;
     }
